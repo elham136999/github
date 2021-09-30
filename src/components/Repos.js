67a6a0bf792fc01a1b66abc25c1c6311a89f593e
elham136999 <elhamkorivand@yahoo.com>
2021-09-30 22:11:56 +0330

    aa

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -4,7 +4,6 @@ import { GithubContext } from "../context/context";
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
-  console.log(repos);
   return <h2>repos component</h2>;
 };
 
