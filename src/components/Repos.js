406cd07394ef2cdc1e01b8ea8dd342ea1ba7a5d4
elham136999 <elhamkorivand@yahoo.com>
2021-10-01 20:22:24 +0330

    repos

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -32,6 +32,21 @@ const Repos = () => {
       return { ...item, value: item.stars };
     })
     .slice(0, 5);
+
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { stargazers_count, name, forks } = item;
+      total.stars[stargazers_count] = { label: name, value: stargazers_count };
+      total.forks[forks] = { label: name, value: forks };
+      return total;
+    },
+    {
+      stars: {},
+      forks: {},
+    }
+  );
+  stars = Object.values(stars).slice(-5).reverse();
+  forks = Object.values(forks).slice(-5).reverse();
   const chartData = [
     {
       label: "html",
@@ -51,10 +66,10 @@ const Repos = () => {
     <section className='section'>
       <Wrapper className='section-center'>
         <Pie3D data={mostUsed}></Pie3D>
-        <Column3D data={chartData} />
+        <Column3D data={stars} />
         <Doughnut2D data={mostPopular} />
         {/* * <ExampleChart data={chartData} /> */}
-        <Bar3D data={chartData} />
+        <Bar3D data={forks} />
       </Wrapper>
     </section>
   );
