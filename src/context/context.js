06654d46affa8214b41eda9011f91b1808e103cd
elham136999 <elhamkorivand@yahoo.com>
2021-10-05 12:38:29 +0330

    search user

--- src/context/context.js
+++ src/context/context.js
@@ -13,8 +13,52 @@ const GithubProvider = ({ children }) => {
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
 
+  const [requests, setRequest] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequest(remaining);
+        if (remaining === 0) {
+          ErrorToggle(true, "no match");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  function ErrorToggle(show = false, msg = "") {
+    setError({ show, msg });
+  }
+
+  const searchGithubUsers = async (user) => {
+    ErrorToggle();
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log(err)
+    );
+    console.log(response);
+    if (response) {
+      setGithubUser(response.data);
+    } else {
+      ErrorToggle(true, "user not found");
+    }
+  };
+  useEffect(checkRequests, []);
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUsers,
+      }}>
       {children}
     </GithubContext.Provider>
   );
